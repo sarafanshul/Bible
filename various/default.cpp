b61ad30d0551355eadde25ee7a83ad3a4091531e
sarafanshul <anshulsaraf3@gmail.com>
2020-09-10 20:23:05 +0530

    default

--- various/default.cpp
+++ various/default.cpp
@@ -1,14 +1,8 @@
-// pragma for fast runtime optimization
-// // #pragma GCC optimize("O3")
 #pragma GCC optimize("Ofast")  
 #pragma GCC target("avx,avx2,fma") 
 #pragma comment(linker, "/stack:200000000")
 #pragma GCC optimize("unroll-loops")
-
-// header(s)
 #include <bits/stdc++.h>
-
-// custom aliases
 #define ALL(x) x.begin(),x.end()
 #define PB push_back
 #define EB emplace_back
@@ -16,15 +10,20 @@
 #define S second
 #define ll long long
 #define MP make_pair
-// // for overflow
 // #define int long long
 // #define MAX LONG_LONG_MAX
 // #define MIN LONG_LONG_MIN
-
-// namespaces
+#ifdef LOCAL // setting up print debugging (yes lol)
+template<class K, class V>ostream& operator<<(ostream&s,const pair<K,V>&p){s<<'<'<<p.x<<','<<p.y<<'>';return s;}
+template<class T, class=typename T::value_type, class=typename enable_if<!is_same<T,string>::value>::type>
+ostream& operator<<(ostream&s,const T&v){s<<'[';for(auto&x:v){s<<x<<", ";}if(!v.empty()){s<<"\b\b";}s<<']';return s;}
+void __prnt(){cerr<<endl;} template<class T, class...Ts>void __prnt(T&&a,Ts&&...etc){cerr<<a<<' ';__prnt(etc...);}
+#define print(...) __prnt(__VA_ARGS__)
+#else
+#define print(...)
+#endif
 using namespace std;
 
-// global declerations
 const size_t MAXN = 1e5 +7;
 
 
@@ -35,13 +34,7 @@ void check(){
 int32_t main(){
 	ios_base::sync_with_stdio(false); cin.tie(NULL); 
 	cin.exceptions(cin.failbit);
-
-	int t;
-	cin >> t;
-
-	while(t--){
-		check();
-	}
-
+	int t;	cin >> t;
+	while(t--)check();
 	return 0;
 }
